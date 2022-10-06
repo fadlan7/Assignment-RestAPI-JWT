d6ec898d8a67e28607575777f895308360aed3f3
fadlan7 <inifadlan7@gmail.com>
2022-10-06 07:49:03 +0700

    move routes

--- app.js
+++ app.js
@@ -2,17 +2,13 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const PORT = 8000;
-const controllers = require('./controllers');
-const authentication = require('./middleware/authentication');
+const routes = require('./routes/routes');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post('/login', controllers.userController.userLogin);
-app.use(authentication);
-app.get('/teams', controllers.teamController.getAllTeams);
-app.get('/teams/:id', controllers.teamController.getTeamById);
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`Running on port: http://localhost:${PORT}`);
